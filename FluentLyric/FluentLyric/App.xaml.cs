32bcf61d612c1286f68f8705258888f94aa7e953
zhh135 <zhh13950510580@outlook.com>
2023-03-25 22:35:43 +0800

    添加项目文件。

---
+++ FluentLyric/FluentLyric/App.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace FluentLyric
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.SizeChanged += SizeChanged;
+
+            appWindow = GetAppWindow(m_window); //Set ExtendsContentIntoTitleBar for the AppWindow not the window
+            appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+            appWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            appWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            rootFrame = m_window.Content as Frame;
+
+            if (m_window.Content == null)
+            {
+                rootFrame = new Frame();
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                m_window.Content = rootFrame;
+                
+            }
+
+            if(rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(Views.ShellPage), args.Arguments);
+            }
+
+            m_window.Activate();
+                       
+        }
+
+        private async void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+
+            var cd = new ContentDialog
+            {
+                Title = "Button Clicker!",
+                Content = "Failed to load Page " + e.SourcePageType.FullName,
+                CloseButtonText = "Ok",
+                DefaultButton = ContentDialogButton.Close
+            };
+            cd.XamlRoot = m_window.Content.XamlRoot;
+            var result = await cd.ShowAsync();
+        }
+
+        private void OnAppUnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void SizeChanged(object sender, WindowSizeChangedEventArgs args)
+        {
+            //Update the title bar draggable region. We need to indent from the left both for the nav back button and to avoid the system menu
+            Windows.Graphics.RectInt32[] rects = new Windows.Graphics.RectInt32[] { new Windows.Graphics.RectInt32(48, 0, (int)args.Size.Width - 48, 48) };
+            appWindow.TitleBar.SetDragRectangles(rects);
+        }
+
+        private AppWindow GetAppWindow(Window window)
+        {
+            IntPtr hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+            return AppWindow.GetFromWindowId(windowId);
+        }
+
+
+        public static Window m_window;
+
+        private Frame rootFrame;
+        private AppWindow appWindow;
+        
+    }
+}
---
+++ FluentLyric/FluentLyric/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+using FluentLyric.Helpers.UI;
+
+
+namespace FluentLyric
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+
+        
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            
+
+
+        }
+    }
+}
